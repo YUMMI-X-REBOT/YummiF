e462654d12b5f4b8f19a4a6ef8b6c009e8594fa2
YUMMI-X-REBOT <132044404+YUMMI-X-REBOT@users.noreply.github.com>
2023-07-09 08:30:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,8 +62,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL","https://t.me/AloneXBots")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/AlonesHeaven")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL","https://t.me/U_F_C_C_H_A_N_N_E_L")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+WXnUkRHx_MA0Njc1")
 SUPPORT_HEHE = SUPPORT_GROUP
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
